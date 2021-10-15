f6622fadd6833ae66df19d398757b808febf1bb8
istmeprincess <88629463+istmeprincess@users.noreply.github.com>
2021-10-15 07:42:11 -0700

    add pretending

---
+++ test/pretendtobe.js
@@ -0,0 +1,9 @@
+// print the outptut
+
+let loggy = "Hey i am a file that is just pretending to be javascript file" //Double quotes and single quotes doesnt matters here.
+console.log(loggy);
+
+// having some mess with my codes xd!
+
+let doggy = 'Why everyone tries to copy me? :(' 
+console.log(doggy);
